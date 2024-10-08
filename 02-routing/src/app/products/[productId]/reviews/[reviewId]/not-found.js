8ef5c2ccb96a7c981a57b68d4ce85d223b5f9c94
Amir Rismaani <amir.rismaani@gmail.com>
2024-10-08 16:06:11 +0330

    not-found page and programatically call notFound function

---
+++ 02-routing/src/app/products/[productId]/reviews/[reviewId]/not-found.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NotFoundReviewPage() {
+    return (
+        <h1>404 Not Found Review Page!</h1>
+    )
+}
--- 02-routing/src/app/products/[productId]/reviews/[reviewId]/page.js
+++ 02-routing/src/app/products/[productId]/reviews/[reviewId]/page.js
@@ -1,7 +1,8 @@
+import { notFound } from 'next/navigation'
 import React from 'react'
 
 function ReviewDetailsPage({ params }) {
-    console.log(params);
+    if (parseInt(params.reviewId) > 100) notFound();
 
     return (
         <h1>Review # {params.reviewId} of Product # {params.productId}</h1>
